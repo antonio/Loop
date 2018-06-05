432eaf97daeee7397b5e5e0fcc893d74f7db2ddf
Antonio Santos <antonio@santosvelasco.com>
2018-06-05 19:47:00 +0200

    Soften integral correction

--- Loop/Managers/LoopDataManager.swift
+++ Loop/Managers/LoopDataManager.swift
@@ -776,7 +776,7 @@ final class LoopDataManager {
             discrepancyGain = 1.0 // high-frequency RC gain, equivalent to Loop 1.5 gain = 1
             persistentDiscrepancyGain = 5.0 // low-frequency RC gain for persistent errors, must be >= discrepancyGain
             correctionTimeConstant = 90.0 // correction filter time constant in minutes
-            carbEffectLimit = 30.0 // reset integral RC if carbEffect over past 30 min is greater than carbEffectLimit expressed in mg/dL
+            carbEffectLimit = 15.0 // reset integral RC if carbEffect over past 15 min is greater than carbEffectLimit expressed in mg/dL
             let sampleTime: Double = 5.0 // sample time = 5 min
             integralForget = exp( -sampleTime / correctionTimeConstant ) // must be between 0 and 1
             integralGain = ((1 - integralForget) / integralForget) *
